ebe01347f4c9dda7a410ddeb57691f2c53b08168
Sadnelak <clartefelicien@hotmail.com>
2021-06-03 15:59:06 +0200

    normalement identique a branche eureka

--- microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
+++ microservice-produits/src/main/java/com/mproduits/configurations/ApplicationPropertiesConfiguration.java
@@ -2,11 +2,14 @@ package com.mproduits.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 @Component
 @ConfigurationProperties("mes-configs")
+@EnableDiscoveryClient
+@RefreshScope
 public class ApplicationPropertiesConfiguration {
 
     private int limitDeProduits;
